80e9051dde3f23c93e699e51b2a5a61cf1eaace9
yosrikhoja <khojayosri@gmail.com>
2025-05-09 23:37:35 +0100

    add provider

--- main.tf
+++ main.tf
@@ -2,16 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
 
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "eu-west-3"
-}
+
 
 resource "aws_instance" "app_server" {
   ami           = "ami-03b82db05dca8118d"
